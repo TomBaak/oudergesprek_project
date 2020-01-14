0d67c562f44224c12698ebb01b34b7addc177b25
Tom Baak <302396215@student.rocmondriaan.nl>
2020-01-14 21:40:33 +0100

    Added: SLB Pages (Missing new invitation)

--- src/Controller/SLBer/SLBController.php
+++ src/Controller/SLBer/SLBController.php
@@ -17,5 +17,15 @@
 		{
 			return $this->render('slb/slb.html.twig');
 		}
+
+        /**
+         * @Route("/slb/uitnodiging/inschrijvingen", name="inschrijvingen")
+         */
+        public function inschrijvingen()
+        {
+
+
+            return $this->render('slb/slb_inschrijvingen.html.twig');
+        }
 	
 	}
